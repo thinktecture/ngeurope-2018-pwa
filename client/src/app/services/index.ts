12faa56dc0cd3d07ec815e7d0eb4f2e9508bcde7
Steffen Jahr <steffen.jahr@thinktecture.com>
2017-12-19 14:59:30 +0100

    Added missing todoservice ref

--- client/src/app/services/index.ts
+++ client/src/app/services/index.ts
@@ -3,11 +3,14 @@ import {ShareService} from './share';
 import {WindowRef} from './windowRef';
 import {DatabaseService} from './base/database.service';
 import {DatabaseServiceRef} from './database.service.ref';
+import {TodoService} from './base/todo.service';
+import {TodoServiceRef} from './todo.service.ref';
 
 export const APP_SERVICES = [
     NotificationService,
     ShareService,
     WindowRef,
     { provide: DatabaseService, useClass: DatabaseServiceRef },
+    { provide: TodoService, useClass: TodoServiceRef },
 ];
 
