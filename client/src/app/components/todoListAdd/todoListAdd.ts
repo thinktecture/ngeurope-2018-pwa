b419dc65949d97084afba7a57c3c662bb7fc65b3
Steffen Jahr <steffen.jahr@thinktecture.com>
2017-12-21 11:42:19 +0100

    Styled add component

--- client/src/app/components/todoListAdd/todoListAdd.ts
+++ client/src/app/components/todoListAdd/todoListAdd.ts
@@ -4,13 +4,17 @@ import {TodoItem} from '../../models/todoItem';
 
 @Component({
     selector: 'todo-list-add',
-    templateUrl: 'todoListAdd.html'
+    templateUrl: 'todoListAdd.html',
+    styleUrls: ['todoListAdd.scss']
 })
 export class TodoListAddComponent {
     @Output() public itemAdded = new EventEmitter<ITodoItem>();
     public item = new TodoItem();
 
     public addItem() {
+        if (!this.item.text) {
+            return;
+        }
         this.itemAdded.emit(this.item);
         this.item = new TodoItem();
     }
