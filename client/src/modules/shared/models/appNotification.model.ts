8d502f1fab7b5b8da5e071eef4f5a7ea27679819
Steffen Jahr <steffen.jahr@thinktecture.com>
2018-01-07 18:10:57 +0100

    Added native notifications

--- client/src/modules/shared/models/appNotification.model.ts
+++ client/src/modules/shared/models/appNotification.model.ts
@@ -1,4 +1,4 @@
-export class Notification {
+export class AppNotification {
     constructor(public title: string, public message: string) {
     }
 }
--- client/src/modules/shared/services/notification.service.ts
+++ client/src/modules/shared/services/notification.service.ts
@@ -1,17 +1,36 @@
 import {FeatureService} from './feature.service';
 import {Subject} from 'rxjs/Subject';
-import {Notification} from '../models/notification.model';
+import {AppNotification} from '../models/appNotification.model';
 import {Injectable} from '@angular/core';
+import {BrowserFeatureKey} from '../models/browserFeatureKey.model';
 
 @Injectable()
 export class NotificationService {
-    public notifications = new Subject<Notification>();
+    public notifications = new Subject<AppNotification>();
 
     constructor(private _featureService: FeatureService) {
 
     }
 
-    public showNotification(title: string, text: string): void {
-        this.notifications.next(new Notification(title, text));
+    public showNotification(title: string, message: string): void {
+        if (!this._featureService.detectFeature(BrowserFeatureKey.NotificationsAPI).supported) {
+            this._showInAppNotification(title, message);
+        } else {
+            Notification.requestPermission().then(permission => {
+                if (permission === 'granted') {
+                    const notification = new Notification(title, {
+                        body: message
+                    });
+                } else {
+                    this._showInAppNotification(title, message);
+                }
+            })
+
+
+        }
+    }
+
+    private _showInAppNotification(title: string, message: string): void {
+        this.notifications.next(new AppNotification(title, message));
     }
 }
