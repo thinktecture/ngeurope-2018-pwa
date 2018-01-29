525722740a84383e71e596125679c434378b6431
Christian Liebel <christian.liebel@thinktecture.com>
2018-01-29 17:38:51 +0100

    Code clean-up

--- client/src/modules/app/services/update.service.ts
+++ client/src/modules/app/services/update.service.ts
@@ -1,22 +1,18 @@
-import {Inject, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {SwUpdate} from '@angular/service-worker';
 import {Subscription} from 'rxjs/Subscription';
 import {NotificationService} from '../../shared/services/notification.service';
-import {WINDOW} from '../../shared/services/window.token';
 
 @Injectable()
 export class UpdateService {
     private _subscription: Subscription;
 
-    constructor(private _swUpdate: SwUpdate, private _notificationService: NotificationService,
-                @Inject(WINDOW) private _window: Window) {
+    constructor(private _swUpdate: SwUpdate, private _notificationService: NotificationService) {
     }
 
     public register(): void {
-        this._subscription = this._swUpdate.available.subscribe(() => {
-                this._notificationService.showNotification('Update available!', 'Please reload to update the application');
-            }
-        );
+        this._subscription = this._swUpdate.available.subscribe(() => this._notificationService
+            .showNotification('Update available!', 'Please reload to update the application');
     }
 
     public unregister(): void {
