22422aa80ea1ed922cdae266498c9bb03663a663
Steffen Jahr <steffen.jahr@thinktecture.com>
2018-01-30 17:07:19 +0100

    Refactoring

--- client/src/modules/shared/services/pushNotification.service.ts
+++ client/src/modules/shared/services/pushNotification.service.ts
@@ -2,13 +2,11 @@ import {Injectable} from '@angular/core';
 import {SwPush} from '@angular/service-worker';
 import {Observable} from 'rxjs/Observable';
 import {ApiService} from './base/api.service';
-import {Subscription} from 'rxjs/Subscription';
 import {of} from 'rxjs/observable/of';
 import {fromPromise} from 'rxjs/observable/fromPromise';
 
 @Injectable()
 export class PushNotificationService {
-    private _subscription: Subscription;
     private _pushSubscription: PushSubscription;
 
     constructor(private _swPush: SwPush, private _apiService: ApiService) {
@@ -16,6 +14,7 @@ export class PushNotificationService {
 
     public register(): void {
         if (!this._swPush.isEnabled) {
+            console.log('Push: ', this._swPush.isEnabled);
             return;
         }
 
@@ -24,9 +23,6 @@ export class PushNotificationService {
     }
 
     public unregister(): Observable<boolean> {
-        if (this._subscription) {
-            this._subscription.unsubscribe();
-        }
         if (this._pushSubscription) {
             return fromPromise(this._pushSubscription.unsubscribe());
         }
