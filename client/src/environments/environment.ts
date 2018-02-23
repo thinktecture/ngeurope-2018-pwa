22f810fb2d4bd44298247027ffc253b8d3bc54e5
Christian Weyer <christian.weyer@thinktecture.com>
2018-02-23 19:55:06 +0100

    Updating API url

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:9090/api/',
+    apiUrl: 'https://tt-pwademo-api.azurewebsites.net/api/',
     push: {
         publicKey: 'BBh_zx5aEnlMyrM8W8anuyxx2ibkb9cUxZclHHDHuBd3uX7PNp-minttLaWe0jpOiHvNfUHXD1rUXTfYf87URlE'
     }
